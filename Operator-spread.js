04d4fd58bfe897bbfc4ec560488e9af92ca680e7
PKS <goldenmaly@naver.com>
2023-03-25 09:58:14 +0900

    rename

---
+++ Operator-spread.js
@@ -0,0 +1,7 @@
+const animals = ['비둘기','닭','토끼']
+const anotherAnimal = [...animals, '오리']
+const test01 = ['강아지', ...animals]
+
+console.log(animals);
+console.log(anotherAnimal);
+console.log(test01);
